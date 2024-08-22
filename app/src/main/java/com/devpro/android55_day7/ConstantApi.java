78add91d75e4e7271decb71e7bbb537e879d9696
Vinh <lvinh90@gmail.com>
2024-08-22 20:28:40 +0700

    - add search logic

--- app/src/main/java/com/devpro/android55_day7/ConstantApi.java
+++ app/src/main/java/com/devpro/android55_day7/ConstantApi.java
@@ -3,4 +3,5 @@ package com.devpro.android55_day7;
 public class ConstantApi {
     public static final String BASE_URL = "https://dummyjson.com/";
     public static final String GET_ALL_PRODUCT = "products";
+    public static final String GET_SEARCH_PRODUCT = "products/search";
 }
--- app/src/main/java/com/devpro/android55_day7/networks/DummyService.java
+++ app/src/main/java/com/devpro/android55_day7/networks/DummyService.java
@@ -5,10 +5,13 @@ import com.devpro.android55_day7.models.AllProductResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface DummyService {
 
     @GET(ConstantApi.GET_ALL_PRODUCT)
     Call<AllProductResponse> getAllProduct();
+    @GET(ConstantApi.GET_SEARCH_PRODUCT)
+    Call<AllProductResponse> searchProduct(@Query("q") String productName);
 
 }
